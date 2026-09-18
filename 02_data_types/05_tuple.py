feba0f3b3cf54fb4e9896f86dbe63c2deac4319d
Roshan Mallick <roshanmallick2025@gmail.com>
2026-09-19 03:01:49 +0530

    Add Python tuple examples

---
+++ 02_data_types/05_tuple.py
@@ -0,0 +1,13 @@
+tuple = (1,2,5)
+
+print(tuple[0])
+
+print(tuple[-1])
+
+print(tuple[0:2])
+
+#tuple[1] = 5 tuple is not immutable so value cant be assing and it will give error
+
+print(tuple[1])
+
+print(len(tuple))
