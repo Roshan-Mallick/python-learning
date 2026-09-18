3481668f10346764f7f6b3e1f354be70b1101a5e
Roshan Mallick <roshanmallick2025@gmail.com>
2026-09-19 03:01:52 +0530

    Add Python operator precedence

---
+++ 04_numbers_in_depth/06_operator_precedence.py
@@ -0,0 +1,27 @@
+ans = 2 + (1 - 3)
+
+print(ans)
+
+ans1 = 2 + 1 - 3
+
+print(ans)
+
+
+ans2 = 2 * (3 + 2 )
+
+print(ans2)
+
+ans3 = 2 * 3 + 2
+
+print(ans3)
+
+ans4 = (2 ** 3) // 2
+
+print(ans4)
+
+
+ans5 = 1 + 2 ** 3 + ( 4 / 2 ) // 3 - 3 / 2 * 5
+
+print(ans5)
+
+print(type(ans5))
