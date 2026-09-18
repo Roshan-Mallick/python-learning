cfa0b36cde55d1b4f882c12b7141dca1acbdd2a4
Roshan Mallick <roshanmallick2025@gmail.com>
2026-09-19 03:01:51 +0530

    Add Python integer concepts

---
+++ 04_numbers_in_depth/02_integers.py
@@ -0,0 +1,31 @@
+Age = 20
+Marks = 95 
+
+print("Positive Integer----------------------->")
+print(Age)
+print(Marks)
+
+
+temperature = -5
+balance = -100
+
+print("Negative Integer---------------------->")
+print(temperature)
+print(balance)
+
+number = 0 
+print("zero----------------------------------->")
+print(number)
+
+x = 1000
+print("Type of the x -------------------------->")
+print(type(x))
+
+
+y = 2 ** x
+
+print(x)
+
+print("Python integers can be very large ----> Large calculations")
+print(y)
+print(type(y))
