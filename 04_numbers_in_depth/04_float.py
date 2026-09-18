b0904e2690248ed8f91d76358482fb43fa8d91bf
Roshan Mallick <roshanmallick2025@gmail.com>
2026-09-19 03:01:52 +0530

    Add Python float concepts

---
+++ 04_numbers_in_depth/04_float.py
@@ -0,0 +1,33 @@
+x = 10.5
+print(x)
+print(type(x))
+
+print(40.5 + 32.5)
+print(1.2 - 23.5)
+print(5.5 * 3.2)
+print(10.6 / 5.6)
+
+#int + float --> float
+
+result = 50 + 4.6
+print(result)
+print(type(result)) 
+
+
+
+
+print("Converting int to float---------> ")
+
+x = float(40)
+
+print(x)
+
+print(type(x))
+
+print("converting float to int ---------->")
+
+y = int(40.123)
+
+print(y)
+
+print(type(y))
