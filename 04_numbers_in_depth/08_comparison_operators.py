1a1d55b7f9f9f30b085e88a7cd03604f022b91c4
Roshan Mallick <roshanmallick2025@gmail.com>
2026-09-19 03:01:53 +0530

    Add Python comparison operators

---
+++ 04_numbers_in_depth/08_comparison_operators.py
@@ -0,0 +1,34 @@
+a = 1 < 2 
+
+print(a)
+
+b = True == 1
+
+print(b)
+
+c = 1 == 1 and 2 <= 2
+
+print(c)
+
+d = False != 0
+
+print(d)
+
+x = 1 
+y = 2
+z = 3
+ 
+ans =  (x < y < z) #chained comparisons used and operator s
+
+print(ans)
+
+print(type(ans))
+
+
+ans1 = x < y and z > y
+
+print(ans1)
+
+ans2 = x < y or z < y
+
+print(ans2)
