9cc0c9687a25368e1fb46fa58130e02bae69fec5
Roshan Mallick <roshanmallick2025@gmail.com>
2026-09-19 03:01:53 +0530

    Add Python bitwise operators

---
+++ 04_numbers_in_depth/10_bitwise_operators.py
@@ -0,0 +1,62 @@
+# ==========================================
+# BITWISE OPERATORS IN PYTHON
+# ==========================================
+
+# 1. Bitwise AND (&)
+
+a = 1 & 2
+b = 1 & 3
+
+# 0 & 0 → 0
+# 0 & 1 → 0
+# 1 & 0 → 0
+# 1 & 1 → 1
+
+print(a)
+print(b)
+print(type(a))
+
+
+# 2. Bitwise OR (|)
+
+c = 5 | 10
+d = 5 | 5
+
+print(c)
+print(d)
+
+
+# 3. Bitwise XOR (^)
+
+e = 5 ^ 5
+f = 4 ^ 6
+
+print(e)
+print(f)
+
+
+# 4. Bitwise NOT (~)
+
+g = ~5
+h = ~10
+
+print(g)
+print(h)
+
+
+# 5. Left Shift (<<)
+
+i = 1 << 2
+j = 5 << 1
+
+print(i)
+print(j)
+
+
+# 6. Right Shift (>>)
+
+k = 8 >> 2
+l = 10 >> 1
+
+print(k)
+print(l)
