9e8090ea8e2f687194e7137f444c31d71a5358c4
Roshan Mallick <roshanmallick2025@gmail.com>
2026-09-19 03:01:52 +0530

    Add Python operator overloading

---
+++ 04_numbers_in_depth/07_operator_overloading.py
@@ -0,0 +1,30 @@
+a = 10 
+b = 20
+
+print(a+b)
+
+first = "Roshan"
+second = " Mallick"
+
+print( first + second )
+
+x = 5 
+y = 3 
+
+print(x * y)
+
+word = "R "
+
+print(word * 3) #repetition
+
+print(10 == 10)
+
+print("python" == "python")
+
+print(2+2) #addition
+
+print("2" + "2") #concatination
+
+
+
+
