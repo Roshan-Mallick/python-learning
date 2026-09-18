e23e67e520fdec56df7eec288e185f4e892d3f9b
Roshan Mallick <roshanmallick2025@gmail.com>
2026-09-19 03:01:49 +0530

    Add Python dictionary examples

---
+++ 02_data_types/04_dictionary.py
@@ -0,0 +1,13 @@
+MyDictionary = {
+
+     'Name' : 'Roshan Mallik' ,
+     'Age'  : 22 ,
+     'branch' : 'CSE',
+     'Number' : 7363053716
+
+}
+
+
+print(MyDictionary['Name'])
+
+print(MyDictionary['Age'])
