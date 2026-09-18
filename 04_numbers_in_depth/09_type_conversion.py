ce5710141d57cee09cb6e018e03466313fa7c157
Roshan Mallick <roshanmallick2025@gmail.com>
2026-09-19 03:01:53 +0530

    Add Python type conversion

---
+++ 04_numbers_in_depth/09_type_conversion.py
@@ -0,0 +1,46 @@
+x = 40
+y = x
+print(y)
+print(type(y))
+
+# int -> float
+y = float(x)
+print(y)
+print(type(y))
+
+
+num = 4.5
+print(num)
+print(type(num))
+
+
+
+# float -> complex
+n = complex(num)
+print(n)
+print(type(n))
+
+
+#int --> bool
+print(bool(1))
+print(bool(0))
+
+#float --> bool
+
+print(bool(1.0))
+print(bool(0.0))
+
+#bool --> float
+
+print(int(True))
+print(int(False))
+
+#bool --> float
+
+print(float(True))
+print(float(False))
+
+#Decimal part is removed when converting float to int 
+
+print(int(9.99))
+print(int (-10.66))
