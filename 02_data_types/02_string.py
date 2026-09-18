b04ff42ac565fae71208f15c74ec4512a07c2b63
Roshan Mallick <roshanmallick2025@gmail.com>
2026-09-19 03:01:48 +0530

    Add Python string examples

---
+++ 02_data_types/02_string.py
@@ -0,0 +1,11 @@
+username = "roshan"
+
+print(len(username))
+
+print(username[0]) # string is nothing but collection of charecter which can be traverse by indexing
+
+#username[0] = 'R'---> this will give error cause string is immutebale
+
+print(username[-1]) #this start count char from last of the string
+
+print(username[1:4]) #it will print from 1th pos to 3th pos 4thpos char will not be included cause it stops at 3th pos
