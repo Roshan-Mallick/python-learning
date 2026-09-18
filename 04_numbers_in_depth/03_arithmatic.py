32ed6a679cb0d90a710c7ed2d4e0b43cc4a32d77
Roshan Mallick <roshanmallick2025@gmail.com>
2026-09-19 03:01:51 +0530

    Add Python arithmetic operations

---
+++ 04_numbers_in_depth/03_arithmatic.py
@@ -0,0 +1,25 @@
+
+
+a = 10
+b = 3
+
+# Addition
+print("Addition:", a + b)
+
+# Subtraction
+print("Subtraction:", a - b)
+
+# Multiplication
+print("Multiplication:", a * b)
+
+# Division
+print("Division:", a / b)
+
+# Floor Division
+print("Floor Division:", a // b)
+
+# Modulus (remainder)
+print("Modulus:", a % b)
+
+# Exponentiation (power)
+print("Exponentiation:", a ** b)
