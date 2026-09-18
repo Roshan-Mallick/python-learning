925406c97e11268e73cff5acb7f7891253649913
Roshan Mallick <roshanmallick2025@gmail.com>
2026-09-19 03:01:52 +0530

    Add Python division and float precision

---
+++ 04_numbers_in_depth/05_division_and_float_precision.py
@@ -0,0 +1,25 @@
+result = 10 / 2
+print(result)
+print(type(result))
+
+answer = 1 / 3.0
+
+print(answer)
+
+print(type(answer))
+
+result = 10 // 2
+
+print(result)
+
+print(type(result))
+
+#float conversion
+
+num = 40 
+
+result = num / 1.0
+
+print(result)
+
+print(type(result))
