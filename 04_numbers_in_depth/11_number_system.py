60638d6d62039170fbb209f2710fe1dcb2f7b33e
Roshan Mallick <roshanmallick2025@gmail.com>
2026-09-19 03:01:54 +0530

    Add Python number systems

---
+++ 04_numbers_in_depth/11_number_system.py
@@ -0,0 +1,31 @@
+# Binary, Octal, Decimal and Hexadecimal
+
+# Binary
+print(0b1000)        # 8
+
+# Octal
+print(0o20)          # 16
+
+# Hexadecimal
+print(0xFF)          # 255
+
+
+# Decimal → other bases
+
+print(bin(64))       # 0b1000000
+print(oct(64))       # 0o100
+print(hex(64))       # 0x40
+
+
+# Other bases → Decimal
+
+print(int("10000", 2))   # 16
+print(int("64", 8))      # 52
+print(int("64", 16))     # 100
+
+
+# Base conversion examples
+
+print(bin(10))
+print(oct(10))
+print(hex(10))
