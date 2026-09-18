78c09c2b8340ce421d4b80e4861c4cf47bd090de
Roshan Mallick <roshanmallick2025@gmail.com>
2026-09-19 03:01:49 +0530

    Add Python list examples

---
+++ 02_data_types/03_list.py
@@ -0,0 +1,15 @@
+list = [123 , "chai" , 3.14] # list can store mutiple types of data in a list
+
+print(list)
+
+print(len(list))
+
+print(list[0])
+ 
+list[0] = 456  ## new values can be assing cause list is mutable
+
+print(list[0])
+
+print(list[-1]) #size - 1 index value
+
+print(list[0:2]) #slicing 
