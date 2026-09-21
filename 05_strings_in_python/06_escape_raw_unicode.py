911731fa8984c2272d8f13ee3df30078899b6083
Roshan Mallick <roshanmallick2025@gmail.com>
2026-09-21 21:40:10 +0530

    Add 05: escape raw unicode

---
+++ 05_strings_in_python/06_escape_raw_unicode.py
@@ -0,0 +1,39 @@
+print("Hello\nRoshan")
+
+print("Hello\tRoshan")
+
+print("I know \"Python\"")
+
+print("c:\\Users\\python")
+
+path = r"C:\Users\Roshan\Python"
+
+print(path)
+
+print("Hello")
+print("চা")
+print("你好")
+print("😀")
+
+print(ord("A"))
+
+print(chr(999))
+
+
+text = "চা"
+
+encoded = text.encode("utf-8")
+
+print(text)
+print(encoded)
+
+decoded = encoded.decode("utf-8")
+
+print(decoded)
+      
+text = "Hello world"
+
+print("Hello" in text)
+print("hello" in text)
+
+print("worldd " not in text)
