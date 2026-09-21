a5f6518824ee2d3a5ef4d0aa372e7e1b2b371526
Roshan Mallick <roshanmallick2025@gmail.com>
2026-09-21 21:40:13 +0530

    Add 06: basic list

---
+++ 06_list_in_python/01_basic_list.py
@@ -0,0 +1,7 @@
+computer_lang = ["python", "java", "c++", "c", "javascript", "golang", "rust"]
+
+print(computer_lang)
+
+print(computer_lang[0])
+
+print(type(computer_lang))  # list is a data type in Python
