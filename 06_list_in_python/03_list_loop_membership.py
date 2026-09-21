9dad9dcbcb79cb035d4ddfe86f8a349f7bc0ab9c
Roshan Mallick <roshanmallick2025@gmail.com>
2026-09-21 21:40:18 +0530

    Add 06: list loop and membership

---
+++ 06_list_in_python/03_list_loop_membership.py
@@ -0,0 +1,19 @@
+computer_lang = ["python", "java", "c++", "c", "javascript", "golang", "rust"]
+
+# Loop through a list
+for i in computer_lang:
+    print(i)
+
+# Print all values in one line
+for i in computer_lang:
+    print(i, end=" ")
+
+print()
+
+# Check whether a value exists
+if "kotlin" in computer_lang:
+    print("kotlin is present in the list")
+
+else:
+    print("kotlin is not present in the list")
+    
