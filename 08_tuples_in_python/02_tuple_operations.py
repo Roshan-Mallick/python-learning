00b9676a9ecb853cf2f574a15d867f9a1cb3600a
Roshan Mallick <roshanmallick2025@gmail.com>
2026-09-21 21:40:40 +0530

    Add 08: tuple operations

---
+++ 08_tuples_in_python/02_tuple_operations.py
@@ -0,0 +1,7 @@
+apple = ("macbook pro", "macbook air", "mac mini", "imac", "mac studio")
+
+windows = ("hp spectre", "dell xps", "lenovo thinkpad", "asus zenbook", "microsoft surface")
+
+all_pc = apple + windows
+
+print(all_pc)
