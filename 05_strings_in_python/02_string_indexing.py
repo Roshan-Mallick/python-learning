1d55ddca6e65f882a29cd6ff26e496caa860b4cd
Roshan Mallick <roshanmallick2025@gmail.com>
2026-09-21 21:39:59 +0530

    Add 05: string indexing

---
+++ 05_strings_in_python/02_string_indexing.py
@@ -0,0 +1,10 @@
+name = 'Roshan'
+print(name)
+
+print("Printed strign char by char ----------------->")
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
