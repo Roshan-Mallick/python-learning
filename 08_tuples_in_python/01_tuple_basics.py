426609f03ae7aebd8b2036701a1ca0b7df011877
Roshan Mallick <roshanmallick2025@gmail.com>
2026-09-21 21:40:38 +0530

    Add 08: tuple basics

---
+++ 08_tuples_in_python/01_tuple_basics.py
@@ -0,0 +1,11 @@
+skills = ("git", "github", "python", "azure", "docker")
+
+print(skills)
+
+print(type(skills))
+
+print(skills[0])  # follows 0-based indexing
+
+print(skills[1:3])
+
+print(len(skills))
