13e03af3efe5442326c1092948709a5b3395d1ab
Roshan Mallick <roshanmallick2025@gmail.com>
2026-09-21 21:41:03 +0530

    Add 09: coffee customization

---
+++ 09_conditional_problems/07_coffe_customizaion.py
@@ -0,0 +1,10 @@
+coffe_size = "medium"
+
+extra_shot = True
+
+if extra_shot:
+   coffe = coffe_size + " coffe with an extra shot"
+else :
+    coffe = coffe_size + " coffe"
+
+print("Order :",coffe)
