7fb47ead0093aade724833959dd0e9b4c03bf3ed
Roshan Mallick <roshanmallick2025@gmail.com>
2026-09-21 21:40:02 +0530

    Add 05: string slicing

---
+++ 05_strings_in_python/03_string_slicing.py
@@ -0,0 +1,14 @@
+full_name = "Roshan Mallick"
+
+print(full_name)
+
+print("Printed name using string slice ---- >")
+print(full_name[0:7])
+
+print(full_name[7:15])
+
+print(full_name[0:8:2])
+
+print(full_name[-7:])
+
+print(full_name[13:6:-1])
