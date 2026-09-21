cab8b5e8f6b7dd1908a62f6419956e039cd82927
Roshan Mallick <roshanmallick2025@gmail.com>
2026-09-21 21:40:05 +0530

    Add 05: string functions

---
+++ 05_strings_in_python/04_string_functions.py
@@ -0,0 +1,27 @@
+full_name = "Roshan MALLICK"
+
+print(full_name)
+
+print(len(full_name))
+
+print(full_name.upper())
+
+print(full_name.lower())
+
+print(full_name.strip())
+
+print(full_name.title())
+
+print(full_name.swapcase())
+
+print(full_name.find("MALLICK"))
+
+user_name = "Jeshan"
+
+print(user_name)
+
+print("Replace : ", full_name.replace("Roshan" , user_name))
+
+skills = ["c lang" , "c++ lang" ,"python"]
+
+print(" ".join(skills))
