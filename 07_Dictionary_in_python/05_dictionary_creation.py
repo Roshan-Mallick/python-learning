9bc6a1db7b695de6a7de42707f3d0dab2eceeb98
Roshan Mallick <roshanmallick2025@gmail.com>
2026-09-21 21:40:35 +0530

    Add 07: dictionary creation

---
+++ 07_Dictionary_in_python/05_dictionary_creation.py
@@ -0,0 +1,17 @@
+# Creating Dictionaries using fromkeys()
+
+key = [
+    "Name",
+    "Age",
+    "is_student",
+    "Country",
+    "Course"
+]
+
+value = None
+new_dictionary1 = {}
+
+# Create dictionary using fromkeys()
+new_dictionary = new_dictionary1.fromkeys(key, value)
+
+print(new_dictionary)
