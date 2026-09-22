ec9a9ff45d98d3a01863fe02eeaebf72a2ee6858
Roshan Mallick <roshanmallick2025@gmail.com>
2026-09-23 02:29:00 +0530

    Add age calculator program and greets user

---
+++ 00_practice/01_greeting.py
@@ -0,0 +1,9 @@
+
+name = str(input("Enter your name : "))
+birth_year = int(input("Enter your birth year : "))
+age = 2026 - birth_year
+
+print(f"Hello {name} , You are {age} years old") 
+
+#f-string (formatted string literal), It allows you to embed variables and expressions directly inside strings using curly braces {}
+
